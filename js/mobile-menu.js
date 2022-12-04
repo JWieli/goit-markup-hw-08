3cca6e46a4f7d60ec8adfad1c89057ae068d26d1
JWieli <113858049+JWieli@users.noreply.github.com>
2022-12-04 15:35:27 +0100

    x

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,11 +1,11 @@
 const menuOverlay = document.querySelector(".mobile-menu");
-const closeMenuButton = document.querySelector(".toggle-menu");
-const openMenuBtn = document.querySelector(".header__link-logo");
+const closeMenuButton = document.querySelector(".mobile-menu__close-button");
+const openMenuBtn = document.querySelector(".mobile-menu__open-button");
 
 openMenuBtn.addEventListener("click", (e) => {
   menuOverlay.classList.remove("d-none");
 });
 
 closeMenuButton.addEventListener("click", (e) => {
-  menuOverlay.classList.add("d-none");
+  menuOverlay0.classList.add("d-none");
 });
